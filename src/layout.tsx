493dd3a47c704f106b22e41caf74aa9ff1c1f674
Abdulwahab O. Jimoh <omoniyi.success@gmail.com>
2024-01-01 05:52:23 -0600

    Re-arrange file structure

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import GoogleAnalytics from './GoogleAnalytics'
+import GoogleAnalytics from '@/src/component/GoogleAnalytics'
 
 const inter = Inter({ subsets: ['latin'] })
 
